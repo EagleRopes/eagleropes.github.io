e6271626ab9a7fef0469775ce82c5c38d0b21608
Luyolo <44893424+yolopalisson@users.noreply.github.com>
2020-04-12 19:50:50 +0200

    Create precache-manifest.789413885364f5471878c18aec976bca.js

---
+++ precache-manifest.789413885364f5471878c18aec976bca.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6607226d133b98befd09bb88dc35513",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b06e99398c59594ae0bc",
+    "url": "./static/css/main.18ebbca6.chunk.css"
+  },
+  {
+    "revision": "7c5ac5e4894138b2e7c9",
+    "url": "./static/js/2.b390c19b.chunk.js"
+  },
+  {
+    "revision": "7cc9bc71dc193dc16a761d4db5c08eb5",
+    "url": "./static/js/2.b390c19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06e99398c59594ae0bc",
+    "url": "./static/js/main.67d8d8f8.chunk.js"
+  },
+  {
+    "revision": "fac73fcdefde17ae5925",
+    "url": "./static/js/runtime-main.4f1a20b4.js"
+  },
+  {
+    "revision": "236fe7fd5925a9c908a0e0c83700814f",
+    "url": "./static/media/BEE 2.236fe7fd.pdf"
+  },
+  {
+    "revision": "60a954ef027d95e6f2af19fa38c7e347",
+    "url": "./static/media/Tax clearance.60a954ef.pdf"
+  },
+  {
+    "revision": "97c5dc37b9293ed0ac08b1f2a8f2bc31",
+    "url": "./static/media/azola.97c5dc37.jpg"
+  },
+  {
+    "revision": "6dc65c8cdf4bd131019ad936d6c6bd8c",
+    "url": "./static/media/boxes.6dc65c8c.jpg"
+  },
+  {
+    "revision": "da5ddfe47912e97ccc180b4093ee342f",
+    "url": "./static/media/counter1.da5ddfe4.jpg"
+  },
+  {
+    "revision": "0f8e4bcc4681de0be6820d54273595da",
+    "url": "./static/media/counter2.0f8e4bcc.jpg"
+  },
+  {
+    "revision": "0a6d035e90ba94db74d884ab3769cbc9",
+    "url": "./static/media/eagle-ropes.0a6d035e.png"
+  },
+  {
+    "revision": "1c710888aeb909eca5faba6a9c6ca377",
+    "url": "./static/media/enza.1c710888.jpg"
+  },
+  {
+    "revision": "3a8478500ebc6b8dee22b7adb0d00905",
+    "url": "./static/media/feltexlogo.3a847850.png"
+  },
+  {
+    "revision": "546cb77cc734ea2b2055b13011f90ca9",
+    "url": "./static/media/kevin.546cb77c.jpg"
+  },
+  {
+    "revision": "881a7a03de4298eb50fddc187fb99291",
+    "url": "./static/media/kitchen.881a7a03.jpg"
+  },
+  {
+    "revision": "34f5cbdc4d7f44b96f9a6ede029429bd",
+    "url": "./static/media/kitchen1.34f5cbdc.jpg"
+  },
+  {
+    "revision": "e31f32ccb81a041938a446f696d1baaa",
+    "url": "./static/media/kzn.e31f32cc.png"
+  },
+  {
+    "revision": "871a6dde808d9b58e37e8dec61f50f48",
+    "url": "./static/media/letterhead.871a6dde.pdf"
+  },
+  {
+    "revision": "bbf2fc02d29a0072cae39ef76ffa566b",
+    "url": "./static/media/mpilo.bbf2fc02.jpg"
+  },
+  {
+    "revision": "95a809a64d2068a07861a73d8b5cf21a",
+    "url": "./static/media/nomfundo.95a809a6.jpg"
+  },
+  {
+    "revision": "5d3630ac9aa30293ca11fc5a88dfe5ef",
+    "url": "./static/media/nopa.5d3630ac.jfif"
+  },
+  {
+    "revision": "6c5016223aa459cffef26df966a624af",
+    "url": "./static/media/office.6c501622.jpg"
+  },
+  {
+    "revision": "9f203a7532ff3d35fe5dc28a2a0af2ce",
+    "url": "./static/media/patrick.9f203a75.jpg"
+  },
+  {
+    "revision": "6210a4ebec77948408b93d7731c40ba1",
+    "url": "./static/media/tables.6210a4eb.jpg"
+  },
+  {
+    "revision": "5485cad3342fa78b3371f24927f629de",
+    "url": "./static/media/transnet.5485cad3.jpg"
+  },
+  {
+    "revision": "95d6466a83c4dda98af1d14cae3ee24f",
+    "url": "./static/media/vodacom-logo.95d6466a.png"
+  },
+  {
+    "revision": "103c65859b41bc502ff5d46db1bf0d00",
+    "url": "./static/media/warehouse.103c6585.jpg"
+  }
+]);
